582997ee29ad8afafdacd3ec55479112f28e068c
Matt <matt@Matts-MacBook-Pro.local>
2018-07-25 15:28:43 -0400

    Removed my semantic library from the git push.

--- webapp.js
+++ webapp.js
@@ -1,20 +1,5 @@
 (function(){  
-    //Array of images to be used and populated.  In 3 columns, rows of 3.  Max limits on H and W.
-    // var imgarray = [
-    //     {caption: "The Pages Fly Away...", src: "surrealart/Book.jpeg"},
-    //     {caption: "Drawn to Life...", src: "surrealart/Draw.jpeg"},
-    //     {caption: "A Fire Inside...", src: "surrealart/Firegirl.jpeg"},
-    //     {caption: "Oceanfront Home...", src: "surrealart/House.jpeg"},
-    //     {caption: "Forgotten Notes...", src: "surrealart/Scissors.jpeg"},
-    //     {caption: "A Stairway to Heaven...", src: "surrealart/Stairway.jpeg"},
-    //     {caption: "A Cosmic Kiss...", src: "surrealart/Cosmokiss.jpeg"},
-    //     {caption: "The Last Parade...", src: "surrealart/Elephant.jpeg"},
-    //     {caption: "The Corridor...", src: "surrealart/Flower.jpeg"},
-    //     {caption: "The Eyes of the Mountain...", src: "surrealart/Mountain.jpeg"},
-    //     {caption: "The Nature of Decay...", src: "surrealart/Skull.jpeg"},
-    //     {caption: "Forced Self-Destruction...", src: "surrealart/Tree.jpeg"},
-    // ]
-
+    
     var imgarray = [
         {caption: "RICK N MORTY", src: "ricknmorty/rnm1.jpeg"},
         {caption: "RICK N MORTY", src: "ricknmorty/rnm2.jpeg"},
@@ -32,33 +17,26 @@
 
     var container = document.querySelector('.maingrid');
     var modal = document.querySelector(".modal");
-    var trigger = document.querySelector(".trigger");
     var closeButton = document.querySelector(".close-button");
     var modalbox = document.getElementById("modalBox")
     var prevButton = document.querySelector(".prev-button");
     var nextButton = document.querySelector(".next-button");
-    var index;
-    var modalimg;
+    var imgAmount = imgarray.length - 1;
 
-    //Global index for my next and prev buttons
-    var globalindex = 0;
-
-
-    var toggleModal = function(event) {
-        index = event.currentTarget.getAttribute('dataIndex');
-        modalimg = document.createElement('img');
-
-        event.preventDefault();
-        modal.classList.toggle("show-modal");
-        globalindex = Number(index);
-        modalimg.setAttribute('src', imgarray[index].src);
+    var populateImage = function(img) {
+        var modalimg = document.createElement('img');
+        modalimg.setAttribute('src', img.src);
         modalimg.classList.add('bigsize');
         modalbox.appendChild(modalimg);
     }
 
-    var hideModal = function(event) {
+    var removeImage = function() {
         var remove = document.querySelector(".bigsize");
         modalbox.removeChild(remove);
+    }
+    
+    var hideModal = function(event) {
+        removeImage();
         modal.classList.toggle("show-modal");
     }
 
@@ -67,119 +45,82 @@
             hideModal();
         }
     }
-
-    var nextIMG = function(event) {
-        var remove = document.querySelector(".bigsize");
-        var modalimg = document.createElement('img');
-        console.log(imgarray.length);
-        if (globalindex % (imgarray.length - 1) === 0 && globalindex != 0) {
-            globalindex = 0;
-        } else {
-            globalindex += 1;
-        };
-        
-        modalbox.removeChild(remove);
-        console.log(globalindex)
-        modalimg.setAttribute('src', imgarray[globalindex].src);
-        modalimg.classList.add('bigsize');
-        modalbox.appendChild(modalimg);
-    }
-
-    var prevIMG = function(event) {
-        var remove = document.querySelector(".bigsize");
-        var modalimg = document.createElement('img');
-        
-        console.log("im working")
-        
-        if (globalindex % (imgarray.length - 1) === 0 && globalindex === 0) {
-            globalindex = 11;
-        } else {
-            globalindex -= 1;
-        };
-        
-        modalbox.removeChild(remove);
-        modalimg.setAttribute('src', imgarray[globalindex].src);
-        modalimg.classList.add('bigsize');
-        modalbox.appendChild(modalimg);
-    }
-
-    prevButton.addEventListener('click', prevIMG);
-    nextButton.addEventListener('click', nextIMG);
-
-    trigger.addEventListener("click", toggleModal);
+    
     closeButton.addEventListener("click", hideModal);
     window.addEventListener("click", windowOnClick);
 
-
-
-
-    //Populates my HTML with images created.  Loops through my array of images.
-    for (var i = 0; i < imgarray.length; i++) {
+    //Main For Loop to Populate Images
+    for (i = 0; i < imgarray.length; i++) {
         (function() {
-            var newimg = document.createElement("img");
-            newimg.classList.add('imgformat');
-            newimg.setAttribute('src', imgarray[i].src);
-            
-
+            var newImg = document.createElement("img");
             var caption = document.createElement("span");
-            // caption.setAttribute('href', "");
-            caption.textContent = imgarray[i].caption;
+            var newDiv = document.createElement("div");
+            var modalDiv = document.createElement("div");
+            var newList = document.createElement("li");
+            var currentIndex = i;
+            var img = imgarray[currentIndex];
+            var nextImage;
+
+            var toggleModal = function(event) {
+                event.preventDefault();
+                modal.classList.toggle("show-modal");
+                populateImage(img);
+            }
+
+            var nextIMG = function(event) {
+                imageScrollR();
+                removeImage();
+                nextImage = imgarray[currentIndex];
+                populateImage(nextImage);
+            }
+
+            var prevIMG = function(event) {
+                imageScrollL();
+                removeImage();
+                nextImage = imgarray[currentIndex];
+                populateImage(nextImage);
+            }
+
+            var imageScrollL = function() {
+                if (currentIndex === 0) {
+                    currentIndex = imgAmount;
+                } else {
+                    currentIndex -= 1;
+                }    
+            };
+        
+            var imageScrollR = function() {
+                if (currentIndex === imgAmount) {
+                    currentIndex = 0;
+                } else {
+                    currentIndex += 1;
+                }
+            };
+
+            newImg.classList.add('imgformat');
+            newImg.setAttribute('src', imgarray[currentIndex].src);
+            
+            caption.textContent = imgarray[currentIndex].caption;
             caption.classList.add('lobstertext', 'links', 'trigger');
             caption.addEventListener('click', toggleModal);
-            caption.setAttribute('dataIndex', i);
 
-            var div = document.createElement("div");
-            div.classList.add('wrapper');
-            div.appendChild(caption);
+            newDiv.classList.add('wrapper');
+            newDiv.appendChild(caption);
 
-            var listitem = document.createElement("li");
-            listitem.classList.add("imgcaption");
+            newList.classList.add("imgcaption");
 
-            var link = document.createElement("div");
-            // link.classList.add('links');
-            // link.setAttribute('href', "");
-            link.appendChild(newimg);
-            
-            var minibox = document.createElement("div");
-            minibox.classList.add("imgcaption");
-            minibox.appendChild(link);
-            minibox.appendChild(caption);
+            modalDiv.classList.add("imgcaption");
+            modalDiv.appendChild(newImg);
+            modalDiv.appendChild(caption);
 
-            container.appendChild(minibox);
+            container.appendChild(modalDiv);
 
-            //variables for my modal box:
+            prevButton.addEventListener('click', prevIMG);
+            nextButton.addEventListener('click', nextIMG);
         }());
     }
     
-
-
-
-
-    // Efforts to get my animated text working.
-    $('.ml2').each(function(){
-        $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
-    });
-
-
-    anime.timeline({loop: true})
-        .add({
-        targets: '.ml2 .letter',
-        scale: [4,1],
-        opacity: [0,1],
-        translateZ: 0,
-        easing: "easeOutExpo",
-        duration: 950,
-        delay: function(el, i) {
-            return 70*i;
-        }
-        }).add({
-        targets: '.ml2',
-        opacity: 0,
-        duration: 1000,
-        easing: "easeOutExpo",
-        delay: 1000
-        });
-    })();
+})();
 
 
 
