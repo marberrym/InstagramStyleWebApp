6574da075c2bb27552217ec52e1de9b8737a408f
Matt <marberrym@gmail.com>
2018-07-26 14:38:26 -0400

    All functionality completed.  Arrow keys or clicks for image scrolling.  Correct sequencing of images.  New background image.  Renamed.  It's looking wonderful.

--- webapp.js
+++ webapp.js
@@ -1,6 +1,5 @@
 (function(){  
     
-
     var imgarray = [
         {caption: "RICK N MORTY", src: "ricknmorty/rnm1.jpeg"},
         {caption: "RICK N MORTY", src: "ricknmorty/rnm2.jpeg"},
@@ -23,6 +22,7 @@
     var prevButton = document.querySelector(".prev-button");
     var nextButton = document.querySelector(".next-button");
     var imgAmount = imgarray.length - 1;
+    var index;
 
     var populateImage = function(img) {
         var modalimg = document.createElement('img');
@@ -46,90 +46,88 @@
             hideModal();
         }
     }
-    
-    closeButton.addEventListener("click", hideModal);
-    window.addEventListener("click", windowOnClick);
+    var nextIMG = function(event) {
+        imageScrollR();
+        removeImage();
+        nextImage = imgarray[index];
+        populateImage(nextImage);
+    }
 
-    //Main For Loop to Populate Images
-    for (i = 0; i < imgarray.length; i++) {
-        (function() {
-            var newImg = document.createElement("img");
-            var caption = document.createElement("span");
-            var newDiv = document.createElement("div");
-            var modalDiv = document.createElement("div");
-            var newList = document.createElement("li");
-            var currentIndex = i;
-            var img = imgarray[currentIndex];
-            var nextImage;
+    var prevIMG = function(event) {
+        imageScrollL();
+        removeImage();
+        nextImage = imgarray[index];
+        populateImage(nextImage);
+    }
 
-            var toggleModal = function(event) {
-                event.preventDefault();
-                modal.classList.toggle("show-modal");
-                populateImage(img);
-            }
+    var imageScrollL = function() {
+        if (index === 0) {
+            index = imgAmount;
+        } else {
+            index -= 1;
+        }    
+    };
 
-            var nextIMG = function(event) {
-                imageScrollR();
-                removeImage();
-                nextImage = imgarray[currentIndex];
-                populateImage(nextImage);
-            }
+    var imageScrollR = function() {
+        if (index === imgAmount) {
+            index = 0;
+        } else {
+            index += 1;
+        }
+    };
 
-            var prevIMG = function(event) {
-                imageScrollL();
-                removeImage();
-                nextImage = imgarray[currentIndex];
-                populateImage(nextImage);
-            }
-
-            var imageScrollL = function() {
-                if (currentIndex === 0) {
-                    currentIndex = imgAmount;
-                } else {
-                    currentIndex -= 1;
-                }    
-            };
-        
-            var imageScrollR = function() {
-                if (currentIndex === imgAmount) {
-                    currentIndex = 0;
-                } else {
-                    currentIndex += 1;
-                }
-            };
-
-            var arrowKeys = function(event) {
-                if (event.key === 'ArrowLeft') {
-                    prevIMG();
-                } else if (event.key === 'ArrowRight') {
-                    nextIMG();
-                }
-            }
-
-            window.addEventListener('keydown', arrowKeys);
-
-            newImg.classList.add('imgformat');
-            newImg.setAttribute('src', imgarray[currentIndex].src);
-            
-            caption.textContent = imgarray[currentIndex].caption;
-            caption.classList.add('lobstertext', 'links', 'trigger');
-            caption.addEventListener('click', toggleModal);
-
-            newDiv.classList.add('wrapper');
-            newDiv.appendChild(caption);
-
-            newList.classList.add("imgcaption");
-
-            modalDiv.classList.add("imgcaption");
-            modalDiv.appendChild(newImg);
-            modalDiv.appendChild(caption);
-
-            container.appendChild(modalDiv);
-
-            prevButton.addEventListener('click', prevIMG);
-            nextButton.addEventListener('click', nextIMG);
-            
-        }());
+    var arrowKeys = function(event) {
+        if (event.key === 'ArrowLeft') {
+            prevIMG();
+        } else if (event.key === 'ArrowRight') {
+            nextIMG();
+        }
     }
     
-})();
+    window.addEventListener('keydown', arrowKeys);
+    closeButton.addEventListener("click", hideModal);
+    window.addEventListener("click", windowOnClick);
+    prevButton.addEventListener('click', prevIMG);
+    nextButton.addEventListener('click', nextIMG);
+
+    //Main For Loop to Populate Images
+    // for (i = 0; i < imgarray.length; i++) {
+    imgarray.forEach(function(image, i) {
+        console.log("I'm running...")
+        var newImg = document.createElement("img");
+        var caption = document.createElement("span");
+        var newDiv = document.createElement("div");
+        var modalDiv = document.createElement("div");
+        var newList = document.createElement("li");
+        
+        var toggleModal = function(event) {
+            event.preventDefault();
+            index = i;
+            modal.classList.toggle("show-modal");
+            populateImage(image);
+        }
+
+        newImg.classList.add('imgformat');
+        newImg.setAttribute('src', image.src);
+        
+        caption.textContent = image.caption;
+        caption.classList.add('lobstertext', 'links', 'trigger');
+        caption.addEventListener('click', toggleModal);
+
+        newDiv.classList.add('wrapper');
+        newDiv.appendChild(caption);
+
+        newList.classList.add("imgcaption");
+
+        modalDiv.classList.add("imgcaption");
+        modalDiv.appendChild(newImg);
+        modalDiv.appendChild(caption);
+
+        container.appendChild(modalDiv);
+
+        
+    });
+}())
+
+    
+    
